2b669cdc9c8adca08e8381a8b4e82a7c7c2c581a
Sudhanshu Rai <sudhanshu3rai@gmail.com>
2025-07-25 19:32:34 +0530

    config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 // next.config.js
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
   },
 };
