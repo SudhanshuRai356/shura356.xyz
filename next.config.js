bfbf7511a485970037e60af9248b9154b83b2ebd
shura356 <sudhanshu3rai@gmail.com>
2025-07-25 18:58:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
